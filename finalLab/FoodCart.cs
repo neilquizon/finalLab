db50e0827441cfff6880faf01174c1854a9bbf31
neilquizon <neil.quizon@gmail.com>
2019-11-29 14:55:29 -0800

    #1 Connected to database and added C# model data

---
+++ finalLab/FoodCart.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace finalLab
+{
+    class FoodCartContext : DbContext
+    {
+        public DbSet<FoodCart> FoodCarts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Data Source =LAPTOP-AS0EL1F8\\SQLEXPRESS; Initial Catalog = FoodCartApp; Integrated Security=true;");
+    }
+
+    class FoodCart
+    {
+
+        [Key]
+        public string key { get; set; }
+
+        public string status { get; set; }
+
+        public string description { get; set; }
+        public string geo_localarea { get; set; }
+        public float longitude { get; set; }
+        public float latitude { get; set; }
+        public string location { get; set; }
+        public string vendor_type { get; set; }
+        public string business_name { get; set; }
+
+    }
+}
--- finalLab/Program.cs
+++ finalLab/Program.cs
@@ -14,17 +14,36 @@ namespace finalLab
         {
             const string URL = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=food-vendors&rows=91";
             string jsonString = CallRestMethod(new Uri(URL));
-            //Rootobject rootObject = JsonConvert.DeserializeObject<Rootobject>(jsonString)
+            Rootobject rootObject = JsonConvert.DeserializeObject<Rootobject>(jsonString);
 
-            Console.WriteLine(jsonString);
-            Console.ReadLine();
-
-
+            //Console.WriteLine(jsonString);
+            //Console.ReadLine();
 
             //now I would convert each record to a FoodCart and add to dtabase...
-            // once all are added I would db.SaveChanges().
+            // once all are added I would db.SaveChanges()
 
+            FoodCartContext db = new FoodCartContext();
+            foreach (Record cart in rootObject.records)
+            {
+                db.Add(new FoodCart
+
+                {
+                    key = cart.fields.key,
+                    status = cart.fields.status,
+                    description = cart.fields.description,
+                    geo_localarea = cart.fields.geo_localarea,                    
+                    longitude = cart.fields.geom.coordinates[0],
+                    latitude = cart.fields.geom.coordinates[1],
+                    location = cart.fields.location,
+                    vendor_type = cart.fields.vendor_type,
+                    business_name = cart.fields.business_name
+
+                   
+                });
+            }
+            db.SaveChanges();
         }
+        
 
         static string CallRestMethod(Uri uri)
         { 
